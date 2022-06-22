37cc21b2d170b61a0cc2f0016c2e998be216fb5f
Mark <mutaimark5@gmail.com>
2022-06-22 14:45:46 +0300

    initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Card } from '@mui/material'
+import {Button} from '@mui/material';
+import './styles.css';
+
+const Form = () => {
+
+  return (
+    <Card className='formone'>
+        <form >
+        <h1>Log In</h1>
+            <div className='inputs'>
+                <label>First Name:
+                    <input type='text' placeholder='Fisrt Name'></input>
+                </label><br></br>
+                <label>Last Name:
+                    <input type='text' placeholder='Last Name'></input>
+                </label><br></br>
+                <label>Username:
+                    <input type='text' placeholder='Username'></input>
+                </label>
+            </div>
+            <div>
+            <Button variant="outlined" color="error" >
+                CANCEL
+            </Button>
+            <Button variant="outlined" type='submit'>
+                SUBMIT
+            </Button>
+            </div>
+        </form>
+    </Card>
+  )
+}
+
+export default Form
